9c25a27656eb3854ff24e925f9d206236f2a9427
rmp9 <raiza.paragguawana@gmail.com>
2023-10-29 19:03:15 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+// Access the global variable ReactDom and render something to the screen like an HTML
+// Put in the root
+ReactDOM.render(<h1>Hello, Everyone</h1>, document.getElementById('root'));
