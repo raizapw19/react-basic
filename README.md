dd438a287aec86fb93feb7872b03f31cf202b978
rmp9 <raiza.paragguawana@gmail.com>
2023-10-29 19:15:41 -0700

    Add README and comments

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a React practice project from the freeCodeCamp.org tutorial
+_React Course - Beginner's Tutorial for React JavaScript Library [2022]_
+by Bob Ziroll, Head of Education at Scrimba
+
+- Using the react, react-dom and Babel CDN
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Access the global variable ReactDom and render something to the screen like an HTML
-// Put in the root
-ReactDOM.render(<h1>Hello, Everyone</h1>, document.getElementById('root'));
+// Place it in the div with the id of root
+// ReactDOM.render(content, position)
+ReactDOM.render(<h1>Hello, Everyone!</h1>, document.getElementById('root'));
